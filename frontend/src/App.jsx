c35f3693b7b45e8ea5ebc544790a9af0be5e8f0d
Ali <ali.derwish@chasacademy.se>
2026-09-09 02:46:13 +0200

    Fixed the routes, pages render separately

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import SubscriptionsPage from './pages/SubscriptionsPage'
 import Login from './pages/Login'
+import Register from './pages/Register'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  
+
 
   return (
     <>
-      <SubscriptionsPage/>
-      <Login/>
+      <Routes>
+       <Route path="/subscriptions" element={<SubscriptionsPage />} />
+       <Route path="/login" element={<Login />} />
+       <Route path="/register" element={<Register />} />
+      </Routes>
+
     </>
   )
 }
