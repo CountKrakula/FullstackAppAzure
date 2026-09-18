7c0358cd5e2eca614d8c85b79d8e9edf8ebc31f3
Ali <ali.derwish@chasacademy.se>
2026-09-18 11:10:04 +0200

    added / route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,12 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
 import NotFound from './pages/NotFound'
+import {Routes, Route, Navigate} from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
